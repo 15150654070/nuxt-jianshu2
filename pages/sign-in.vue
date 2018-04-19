0140c1e908d2134e7e7f9cddeb8b92415cbec4eb
pengpengge <1606368737@qq.com>
2018-04-19 11:59:35 +0800

    登录注册页面布局

--- pages/sign-in.vue
+++ pages/sign-in.vue
@@ -1,9 +1,9 @@
-<template xmlns="http://www.w3.org/1999/html">
+<template>
     <div class="sign">
         <!--logo-->
         <div class="logo">
             <nuxt-link to="/">
-                <img src="~/assets/img/logo.png" alt="">
+                <img src="~/assets/img/logo.png">
             </nuxt-link>
         </div>
         <!--主体部分-->
@@ -13,57 +13,58 @@
                 <h4>
                     <nuxt-link class="active" to="/sign-in">
                         登录
-                    </nuxt-link>·
+                    </nuxt-link>
+                    <b>·</b>
                     <nuxt-link class to="/sign-up">
                         注册
                     </nuxt-link>
                 </h4>
             </div>
             <!--form表单-->
-             <div class="js-form">
-                 <form method="post">
-                     <div class="input-prepend">
-                     <input type="text"  placeholder="手机号或邮箱">
-                     <i class="fa fa-user"></i>
-                     </div>
-                     <div class="input-prepend">
-                     <input type="text"  placeholder="密码">
-                     <i class="fa fa-lock"></i>
-                     </div>
-                     <div class="remember">
-                         <input   type="checkbox">
-                         <span>记住我</span>
-                     </div>
-                         <nuxt-link class="help" to="/">登录遇到问题？</nuxt-link>
-                         <button type="button" class="btn btn-primary"> 登录</button>
-
-
-
-                     <!--第三方登录-->
-                     <div class="more-sign">
-                         <p class="">社交账号登录</p>
-                         <ul class="">
-                             <li>
-                             <nuxt-link to="/">
-                                 <i class="fa fa-weixin"></i>
-                             </nuxt-link>
-                             </li>
-                             <li>
-                                 <nuxt-link to="/">
-                                     <i class="fa fa-weibo"></i>
-                                 </nuxt-link>
-                             </li>
-                             <li>
-                                 <nuxt-link to="/">
-                                     <i class="fa fa-qq"></i>
-                                 </nuxt-link>
-                             </li>
-                         </ul>
-                     </div>
-                 </form>
-             </div>
+            <div class="js-form">
+                <form method="post">
+                    <div class="input-prepend">
+                        <input type="text"  placeholder="手机号或邮箱">
+                        <i class="fa fa-user"></i>
+                    </div>
+                    <div class="input-prepend">
+                        <input type="text"  placeholder="密码">
+                        <i class="fa fa-lock"></i>
+                    </div>
+                    <div class="remember">
+                        <input type="checkbox">
+                        <span>记住我</span>
+                    </div>
+                    <div class="help">
+                        <nuxt-link to="/">
+                            登录遇到问题?
+                        </nuxt-link>
+                    </div>
+                    <button class="sign-in-btn">登录</button>
+                </form>
+            </div>
+            <!--第三方的登录-->
+            <div class="more-sign">
+                <p>社交账号登录</p>
+                <ul>
+                    <li>
+                        <nuxt-link to="/">
+                            <i class="weixin fa fa-weixin"></i>
+                        </nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link to="/">
+                            <i class="weibo fa fa-weibo"></i>
+                        </nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link to="/">
+                            <i class="qq fa fa-qq"></i>
+                        </nuxt-link>
+                    </li>
+                </ul>
+            </div>
         </div>
-        
     </div>
 </template>
 <script>
